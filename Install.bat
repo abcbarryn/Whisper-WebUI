e46fd9bab0fe344a8b9b921ccc5c6de7cbe20a78
Barry Nelson <abcbarryn@users.noreply.github.com>
2026-03-11 17:26:39 -0400

    Added constraint.txt to keep Gradio version <= 5.34.0
    Added constraint.txt to keep Gradio version <= 5.34.0 and updated requirements to allow up to 5.34.0

--- Install.bat
+++ Install.bat
@@ -9,7 +9,7 @@ echo checked the venv folder. now installing requirements..
 call "%~dp0\venv\scripts\activate"
 
 python -m pip install -U pip wheel
-pip install --no-build-isolation -r requirements.txt
+pip install --no-build-isolation -c constraint.txt -r requirements.txt
 
 if errorlevel 1 (
     echo.
--- Install.sh
+++ Install.sh
@@ -8,7 +8,7 @@ fi
 source venv/bin/activate
 
 python -m pip install -U pip wheel
-pip install --no-build-isolation -r requirements.txt && echo "Requirements installed successfully." || {
+pip install --no-build-isolation -c constraint.txt  -r requirements.txt && echo "Requirements installed successfully." || {
     echo ""
     echo "Requirements installation failed. Please remove the venv folder and run the script again."
     deactivate
